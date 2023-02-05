40ef43a6e97aef94a746ed8c7911c8e84b9e33fa
nguyenviethuy97 <tuwnguyen@gmail.com>
2023-02-05 23:19:42 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// import { Company } from './Company';
+import { User } from './User';
+import { CustomMap } from './CustomMap';
+import { Company } from './Company';
+
+const customMap = new CustomMap('map');
+customMap.addMarker(new User());
+customMap.addMarker(new Company());
